5c0e32ce6501d3dff7cb635568d0ad837d215696
kamilmuzyka <kamil.muzyka@gmail.com>
2022-01-04 04:49:07 +0100

    5-redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import CounterContextProvider from './4-context/provider'; /** For 4-context only. */
-import { Provider } from 'react-redux'; /** For 5-redux only. */
-import store from './5-redux/store'; /** For 5-redux only. */
+import { Provider } from 'react-redux';
+import store from './counter/store';
 
 ReactDOM.render(
     <React.StrictMode>
-        {/** For 4-context only. */}
-        <CounterContextProvider>
-            {/** For 5-redux only. */}
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </CounterContextProvider>
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
